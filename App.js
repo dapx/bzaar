736f31094d3e5e5327d4e3c2cfa735227a40b5ce
Daniel André <dap1995@gmail.com>
2017-07-29 08:22:54 -0300

    Fix expo load font

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
+import Expo from "expo";
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Bzaar from './js/app.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Bzaar />
-    );
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
   }
 
   async componentDidMount() {
-      await Expo.Font.loadAsync({
-        'Roboto': require('native-base/Fonts/Roboto.ttf'),
-        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-        'FontAwesome': require('native-base/Fonts/FontAwesome.ttf'),
-      });
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'FontAwesome': require('native-base/Fonts/FontAwesome.ttf'),
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <Text>Loading Fonts</Text>;
+    }
+    return <Bzaar />;
   }
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
