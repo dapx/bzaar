e995abf250b43ca31113d95a6dbd2493a87a74c7
Daniel André da Silva <dap1995@gmail.com>
2018-02-22 00:11:30 -0300

    Added eslint react-native plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import",
-    ],
-    "rules": {
-	"react/jsx-filename-extension": "off",
-	"import/prefer-default-export": "off",
-    },
-    "globals": { "fetch": false }
+  "extends" : [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb-base"
+  ],
+  "plugins": [
+    "react",
+    "react-native",
+    "jsx-a11y",
+    "import",
+  ],
+  "rules": {
+    "react/jsx-filename-extension": "off",
+    "import/prefer-default-export": "off",
+    "arrow-body-style" : "warn",
+    "class-methods-use-this": "off",
+  },
+  "globals": { "fetch": false }
 };
