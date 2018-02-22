282377bdd60716b5e9d9e1d9e1f6a2f9548be47c
Daniel André da Silva <dap1995@gmail.com>
2018-02-22 00:13:41 -0300

    Fix identation and added PropTypes to HeaderBack component

--- js/components/headerBack.js
+++ js/components/headerBack.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 export default class HeaderBack extends Component {
 
-	render() {
-		return (
-			<Header>
-				<Left>
-					<Button transparent onPress={_.throttle(this.props.back, 500)}>
-						<Icon style={{ color: 'black' }} name="arrow-left" />
-					</Button>
-				</Left>
-				<Body>
-					<Title style={{ color: 'black' }}>{this.props.title}</Title>
-				</Body>
-				<Right />
-			</Header>
-		);
-	}
+  render() {
+    return (
+      <Header>
+        <Left>
+          <Button transparent onPress={_.throttle(this.props.back, 500)}>
+            <Icon style={{ color: 'black' }} name="arrow-left" />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={{ color: 'black' }}>{this.props.title}</Title>
+        </Body>
+        <Right />
+      </Header>
+    );
+  }
 }
+
+HeaderBack.propTypes = {
+  back: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+};
